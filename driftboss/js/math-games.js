9592bd416c40f7deccce6f631a2217c569b33397
marco48vx <marcovilcans@outlook.com>
2024-10-15 14:34:29 -0400

    Add files via upload

---
+++ driftboss/js/math-games.js
@@ -0,0 +1,12 @@
+let mathGames = [
+	"Drift Boss",
+	"OvO",
+	"Run",
+	"Geometry Dash",
+	"Idle Breakout",
+	"Snow Rider 3D",
+	"Escape",
+	"Papas Freezeria",
+	"Wheelie Bike",
+	"Papas Scooperia"
+];
